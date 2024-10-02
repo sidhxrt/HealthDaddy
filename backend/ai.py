d4b21b836ab1040e0ee315c5248fabc9eb0bd0f3
sidhxrt <sidharthh1880@gmail.com>
2024-10-02 15:33:05 +0530

    fixes

--- backend/ai.py
+++ backend/ai.py
@@ -26,42 +26,7 @@ len(all_docs)
 vectorstore = Chroma.from_documents(documents=docs, embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
 
 
-'''def productInfo(person_info, ingredients):
-    if (ingredients != ""):
-        parser = StrOutputParser()
-        llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.2, max_toxens=500)
-        system_prompt = (
-            "As a certified nutritionist, you are to evaluate a specific food product based on the following standards."
-            "I will provide information about an individual's dietary preferences and a list of ingredients for the product in question. "
-            "Please compare the ingredient list with the established standards and provide the following in the exact format specified:"
-            "safety_score = [one percentage value in array],"
-            "caution_message = ['caution message'],"
-            "short_term_effects = ['effect1', 'effect2', 'effect3'],"
-            "long_term_effects = ['effect1', 'effect2', 'effect3'],"
-            "environmental_score = [one percentage value in array]"
-            "If any of the information is not available, please return 'N/A' or 'None.'"
-        )
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system_prompt),
-                ("user", "{input}"),
-            ]
-        )
 
-        chain = prompt | llm | parser
-        return chain.invoke({"input": ingredients})
-    
-    else:
-        return "['couldnt fetch information for the product']"'''
-
-
-  
-
-
-
-
-
-
-
+# rag
 
 
