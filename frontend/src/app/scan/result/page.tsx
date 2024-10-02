6e75691cf23e9e8d3aff3e94286625ef9f26747d
anushkakathija <anushkakathijaks@gmail.com>
2024-10-02 22:02:59 +0530

    frontend: result page

--- frontend/src/app/scan/result/page.tsx
+++ frontend/src/app/scan/result/page.tsx
@@ -1,6 +1,17 @@
 "use client";
 import { getScoreResultModel } from "@/api/getScore";
-import { List, ListItemText, Typography } from "@mui/material";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Box,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  Stack,
+} from "@mui/material";
 
 export default function Result({
   searchParams,
@@ -9,11 +20,43 @@ export default function Result({
 }) {
   return (
     <>
-      <Typography>Result</Typography>
-      <List>
-        <ListItemText>Score:{searchParams.score}</ListItemText>
-        <ListItemText>Caution:{searchParams.caution}</ListItemText>
-      </List>
+      <ListItem>
+        <ListItemText
+          primary={`${searchParams.score}`}
+          primaryTypographyProps={{
+            sx: {
+              fontWeight: "bold",
+              color: "#FFFFF",
+              fontSize: "30px",
+              padding: "40px",
+              backgroundColor: "#6fc276",
+              borderRadius: "8px",
+              width: "100%",
+              textAlign: "centre",
+            },
+          }}
+        />
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary={`Caution: ${searchParams.caution}`}
+          primaryTypographyProps={{
+            sx: {
+              fontStyle: "italic",
+              color: "red",
+              fontSize: "20px",
+              fontWeight: "bold",
+              gap: "50px",
+              padding: "30px",
+              backgroundColor: "#ffee8c",
+              borderRadius: "8px",
+              width: "100%",
+              margin: "0 auto",
+              textAlign: "centre",
+            },
+          }}
+        />
+      </ListItem>
     </>
   );
 }
