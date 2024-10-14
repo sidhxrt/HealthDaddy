e56d945ac05be72a8fae7a6483a4bf819dbd376e
EffDuBois <arnabkar2003@protonmail.com>
2024-10-14 17:29:44 +0530

    Proxy fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
+    async rewrites() {
         return [
             {
-                permanent: true,
                 source: "/localhost/:path*",
                 destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
             }
